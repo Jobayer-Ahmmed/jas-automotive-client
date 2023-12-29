9446e427403c6b6f0d9cd8e9da67cdc790b27727
%Sa2323ma <jobayer.ahmmed.jas@gmail.com>
2023-12-29 21:09:51 +0600

    edit : admin system

--- src/component/shared/navbar/Navbar.jsx
+++ src/component/shared/navbar/Navbar.jsx
@@ -4,49 +4,53 @@ import { useContext } from "react";
 import { MyContext } from "../../contextApi/MyAuthProvider";
 import { useState } from "react";
 import { useEffect } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Navbar = () => {
   const { myUser, myLogOut } = useContext(MyContext);
-  const email = myUser?.email
-  console.log(email)
-  const [theme, setTheme] = useState("light")
-  
-  const navlinks = <>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/add-product">Add Product</NavLink>
-            </li>
-            <li>
-              <NavLink to="/see-all-car">See All Car</NavLink>
-            </li>
-            <li>
-              <NavLink to={`/my-cart`}>My Cart</NavLink>
-            </li>
-  </>
+  const email = myUser?.email;
+  console.log(email);
+  const [theme, setTheme] = useState("light");
 
-  const handleToggle=e=>{
-    if(e.target.checked){
-      setTheme("dark")
+  const navlinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+
+      {email === "admin@admin.com" && (
+        <li>
+          <NavLink to="/add-product">Add Product</NavLink>
+        </li>
+      )}
+
+      <li>
+        <NavLink to="/see-all-car">See All Car</NavLink>
+      </li>
+      <li>
+        <NavLink to={`/my-cart`}>My Cart</NavLink>
+      </li>
+    </>
+  );
+
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
     }
-    else{
-      setTheme("light")
-    }
-  }
-  useEffect(()=>{
-    localStorage.setItem("theme", theme)
-    const localTheme = localStorage.getItem("theme")
-    document.querySelector("html").setAttribute("data-theme",localTheme)
-  },[theme])
-  
+  };
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+
   const handleLogout = () => {
     myLogOut()
-      .then(() =>{
-        toast.success("You are successfully logout")
+      .then(() => {
+        toast.success("You are successfully logout");
       })
       .catch((err) => console.log(err.message));
   };
@@ -74,47 +78,43 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-          {navlinks}
+            {navlinks}
           </ul>
         </div>
         {/* for logo */}
         <div>
           <span className="text-2xl font-bold">
-            <img src={logo} className=" w-36" /> 
+            <img src={logo} className=" w-36" />
           </span>
         </div>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-        {navlinks}
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navlinks}</ul>
       </div>
       <div className="navbar-end">
         {myUser ? (
           <div className="">
             <div className="dropdown dropdown-end">
               <label tabIndex={0}>
-                      <img className="w-10 h-10 hover:w-11 hover:h-11 rounded-full" src={myUser.photoURL} alt="" />
+                <img
+                  className="w-10 h-10 hover:w-11 hover:h-11 rounded-full"
+                  src={myUser.photoURL}
+                  alt=""
+                />
               </label>
               <ul
                 tabIndex={0}
                 className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <li className="pl-4">
-                 {myUser.displayName}
-                </li>
+                <li className="pl-4">{myUser.displayName}</li>
                 <li>
-                <button
-                      onClick={handleLogout}
-                    >
-                      Log Out
-                    </button>
+                  <button onClick={handleLogout}>Log Out</button>
                 </li>
               </ul>
             </div>
           </div>
         ) : (
-          <ul className="flex gap-6">
+          <ul className="flex gap-2 md:gap-6">
             <li>
               <Link to="/login">Login</Link>
             </li>
@@ -124,36 +124,42 @@ const Navbar = () => {
           </ul>
         )}
 
+        <label className="ml-4 swap swap-rotate">
+          {/* this hidden checkbox controls the state */}
+          <input type="checkbox" onChange={handleToggle} />
 
+          {/* sun icon */}
+          <svg
+            className="swap-on fill-current w-10 h-10"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+          </svg>
 
-        <label className="swap swap-rotate">
-  
-  {/* this hidden checkbox controls the state */}
-  <input type="checkbox" onChange={handleToggle}/>
-  
-  {/* sun icon */}
-  <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-  
-  {/* moon icon */}
-  <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
-  
-</label>
+          {/* moon icon */}
+          <svg
+            className="swap-off fill-current w-10 h-10"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+          </svg>
+        </label>
       </div>
 
       <ToastContainer
-            position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-        />
-
-
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
