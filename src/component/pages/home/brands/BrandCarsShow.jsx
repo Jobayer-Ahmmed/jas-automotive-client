744a0f46667929d534570e1209df252d20b43200
%Sa2323ma <jobayer.ahmmed.jas@gmail.com>
2024-01-08 23:16:58 +0600

    edit : brandcarshow

--- src/component/pages/home/brands/BrandCarsShow.jsx
+++ src/component/pages/home/brands/BrandCarsShow.jsx
@@ -18,13 +18,16 @@ const BrandCarsShow = () => {
 
   return (
     <div className=" mb-myMargin px-6 md:px-xPadding">
-        <Slider/>
+      {
+        carsData.length?<Slider/>:''
+      }
+        
         <h1 className=" text-3xl font-bold text-center mt-myMargin mb-mtMargin">
                 Automotive cars of {brand} 
         </h1>
         {
           carsData.length? '':    
-                  <div className=" bg-textColor text-deepGray text-2xl flex justify-center items-center">
+                  <div className=" bg-textColor text-deepGray text-2xl flex justify-center items-center h-[50vh]">
                     <h3>There is no car of <span className="font-bold">{brand}</span> at now.</h3>
                   </div>
         }
